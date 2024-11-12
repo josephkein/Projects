7ded009fb2dae650da1c9fdd2e2745f30b6d21e7
Josepkein <josephkeinhonrada@gmail.com>
2024-11-12 22:12:44 +0800

    update

--- Portfolio/yes.js
+++ Portfolio/yes.js
@@ -1,11 +1,26 @@
 var menu = document.getElementById('menu');
+var menuBar = document.getElementById('menav');
 
 menu.addEventListener('click', function(){
-    let menuBar = document.getElementById('menav');
 
    if (menuBar.style.display != 'none'){
         menuBar.style.display = 'none';
    } else {
         menuBar.style.display = 'block';
    }
+});
+
+let homePage = document.getElementById('menH');
+let aboutPage = document.getElementById('menA');
+let skillsPage = document.getElementById('menS');
+
+
+homePage.addEventListener('click', function(){
+     menuBar.style.display = 'none';
+});
+aboutPage.addEventListener('click', function(){
+     menuBar.style.display = 'none';
+});
+skillsPage.addEventListener('click', function(){
+     menuBar.style.display = 'none';
 });
