ff942565fe81576d2ccd12137ebda53f0631bd50
Josepkein <josephkeinhonrada@gmail.com>
2026-02-14 00:18:27 +0800

    lesgo

--- jj/assets/main.js
+++ jj/assets/main.js
@@ -1,15 +1,36 @@
 
 let form = document.getElementById("unlock");
+let err = document.getElementById("err");
+let over = document.getElementById("overlay");
+let alert = document.getElementById("alert");
+let ok = document.getElementById("ok");
+
+document.getElementById("code").addEventListener('keydown', (e) => {
+    err.style.display = "none";
+});
+let aud = new Audio('./music/dry-fart.mp3');
+let aud2 = new Audio('./music/wow.mp3');
+over.style.display = "none";
+alert.style.display = "none";
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
     let code = document.getElementById("code").value;
-
-    if (code == "love"){        
-        window.location.href = "jj.html";
+    if (code == "buldagol"){
+        over.style.display = "flex";
+        alert.style.display = "flex";
+        aud2.play();
+        // window.location.href = "jj.html";
+    }
+    else{
+        err.style.display = "flex";
+        aud.play();
     }
 });
+
+function navigate(){
+    window.location.href = "jj.html";
+}
 
 setInterval(hearts, 1000);
 
@@ -28,3 +49,6 @@ setInterval(hearts, 1000);
         }, 10000)
         
     }
+
+
+
